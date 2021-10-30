d5299edbaaa7c7b7df6627fda10c989a5c04e61a
Andrey Azevedo <andreyaze@gmail.com>
2021-10-30 18:02:05 -0300

    adiciona styled-components

--- src/app.js
+++ src/app.js
@@ -1,20 +1,42 @@
+import styled, { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    color: red;
+    padding: 0;
+    margin: 0;
+  }
+
+  h1 {
+    padding: 0;
+    margin: 0;
+  }
+`
+
+const Background = styled.div`
+  display: flex;
+  justify-content: center;
+  background: #ddd;
+  height: 100vh;
+`;
+
+const Main = styled.main`
+  display: flex;
+  background: #fff;
+  width: 100%;
+  max-width: 1200px;
+  max-height: 100vh;
+  margin: 80px 0;
+`;
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+     <GlobalStyle />
+    <Background>
+      <Main />
+    </Background>
+    </>
   );
 }
 
